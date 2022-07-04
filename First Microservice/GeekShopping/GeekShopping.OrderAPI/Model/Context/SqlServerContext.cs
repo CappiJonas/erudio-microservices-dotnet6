9591290551816b22d2098a37c1dc99f3cf4d89ba
Jonas Cappi <cappijonas@gmail.com>
2022-07-04 18:51:07 -0300

    Implementação do microsserviço de pedido utilizando o consumo do RabbitMq

---
+++ First Microservice/GeekShopping/GeekShopping.OrderAPI/Model/Context/SqlServerContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.OrderAPI.Model.Context
+{
+    public class SqlServerContext : DbContext
+    {
+        public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options) { }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+    }
+}
